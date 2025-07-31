af51d83ccc78219390dad5d84f2a51c9ddfe9e4b
Afebu Balogun <balogunafebu22@gmail.com>
2025-07-31 22:51:54 +0100

    fix: reverted cors config

--- main.py
+++ main.py
@@ -27,18 +27,9 @@ app = FastAPI(lifespan=lifespan)
 limiter = Limiter(key_func=get_remote_address)
 app.state.limiter = limiter
 
-origins = [
-    "http://localhost:3000",
-    "http://localhost:3001",
-    "http://localhost:5173",
-    "http://localhost:5174",
-    "http://localhost:5175",
-    "https://ican-testa-examhack.vercel.app"
-]
-
 app.add_middleware(
 	CORSMiddleware,
-	allow_origins=origins,
+	allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
